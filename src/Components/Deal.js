74ebdda5c4f72a1c0b9da1b01c7416abfb5c4d7d
SlaviManzurov <77936591+SlaviManzurov@users.noreply.github.com>
2021-04-03 17:25:54 +0300

    Make deal layout and css

--- src/Components/Deal.js
+++ src/Components/Deal.js
@@ -1,6 +1,12 @@
+import './Deal.css'
+
 function Deal() {
     return(
-        <div>Deal</div>
+        <div className="deal">
+            <img src="https://cached.imagescaler.hbpl.co.uk/resize/scaleWidth/815/cached.offlinehbpl.hbpl.co.uk/news/ORP/Coke-20150730041122131.JPG" alt=""/>
+            <h3>DealName</h3>
+            <span>Price</span><span>discount</span><span>Untile</span>
+        </div>
     )
 }
 
--- src/Components/DealsPage.js
+++ src/Components/DealsPage.js
@@ -5,6 +5,7 @@ function DealsPage() {
     return (
         <div className='background-img'>
             <Deal/>
+            <Deal/>
         </div>
     )
 }
