3d68c700fa757ffe614f8478b9911852bc6c73f3
SlaviManzurov <77936591+SlaviManzurov@users.noreply.github.com>
2021-05-09 17:30:42 +0300

    CreateUser

--- src/Components/Register.js
+++ src/Components/Register.js
@@ -1,25 +1,42 @@
+import firebase from '../utils/firebase'
+
 import './Register.css'
+
 
 function Register() {
 
     const onRegisterSubmitHandler = (e) => {
         e.preventDefault();
+
+        const username = e.target.username.value
+        const password = e.target.password.value
+        const rePassword = e.target.rePassword.value
+
+        console.log(username, password, rePassword);
+
+        if (password === rePassword) {
+            firebase.auth().createUserWithEmailAndPassword(username, password)
+                .then((userCredential) => {
+                    console.log(userCredential);
+                })
+        }
+
     }
-    return(
+    return (
         <div className='form'>
             <form onSubmit={onRegisterSubmitHandler}>
                 <h1>Register</h1>
 
                 <label htmlFor="username">Username</label>
-                <input type="text"></input>
+                <input type="text" name='username'></input>
 
                 <label htmlFor="password">Password</label>
-                <input type="password"></input>
+                <input type="password" name='password'></input>
 
                 <label htmlFor="rePassword">Repeat Password</label>
-                <input type="password"></input>
+                <input type="password" name='rePassword'></input>
                 <p>
-                <input className='button' type="submit" value='Register'></input>
+                    <input className='button' type="submit" value='Register'></input>
                 </p>
 
             </form>
