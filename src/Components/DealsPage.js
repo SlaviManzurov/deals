3b5abe650acfb8f7c26374296f32b299f8ae7630
SlaviManzurov <77936591+SlaviManzurov@users.noreply.github.com>
2021-05-20 18:57:04 +0300

    DealsPage  Map

--- src/Components/DealsPage.js
+++ src/Components/DealsPage.js
@@ -1,11 +1,22 @@
+import { useState, useEffect } from 'react'
 import Deal from './Deal'
 import * as dealServices from './services/dealServices'
 
 function DealsPage() {
-    dealServices.getAll().then(res => console.log(res))
+
+    const [deals, setDeals] = useState([])
+
+    useEffect(() => {
+        dealServices.getAll()
+            .then(res => setDeals(res))
+    }, [])
+
+    console.log(deals);
     return (
         <div>
-            <Deal store='tesco' dealName ='cola' untile = '20.05.2021' price = '1' discount = '0.5' imageUrl = 'https://upload.wikimedia.org/wikipedia/commons/thumb/f/f6/15-09-26-RalfR-WLC-0098.jpg/170px-15-09-26-RalfR-WLC-0098.jpg' />
+            {Object.entries(deals).map(([key,value]) =>
+                <Deal key={key} {...value}/>
+            )}
         </div>
     )
 }
