92f83c672d0d16b1dd74419c1bace00078e448d0
SlaviManzurov <77936591+SlaviManzurov@users.noreply.github.com>
2021-11-01 15:32:30 +0200

    href address

--- src/Components/Deal.js
+++ src/Components/Deal.js
@@ -16,7 +16,7 @@ function Deal({
             <h2>{dealName}</h2>
             <div>
                 <h3>{store}</h3>
-                <p>{address}</p>
+                <a href={`https://www.google.com/maps/search/?api=1&query=${address}`} target="_blank">{address}</a>
             </div>
             <div className ="price">
                 <p><strong>Price</strong></p>
